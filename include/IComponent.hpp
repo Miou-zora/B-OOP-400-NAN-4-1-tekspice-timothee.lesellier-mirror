9d0f827ce4a2701cee482a5caaf5e4976fbba8e9
Timothée Lesellier <timothee.lesellier@epitech.eu>
2023-02-07 09:35:00 +0100

    feat(core): adding the enum Tristate in the namespace nts

--- include/IComponent.hpp
+++ include/IComponent.hpp
@@ -10,13 +10,21 @@
 
 namespace nts
 {
+    // Types from the namespace
+    enum Tristate {
+        Undefined = (-true),
+        True = true,
+        False = false
+    };
+
+    // Class from the namespace
     class IComponent {
     public:
             IComponent() = default;
             virtual ~IComponent() = default;
 
             virtual void simulate(std::size_t tick) = 0;
-            //virtual nts::Tristate compute(std::size_t pin) = 0;
+            virtual nts::Tristate compute(std::size_t pin) = 0;
             virtual void setLink(std::size_t pin, nts::IComponent& other, std::size_t otherPin) = 0;
     };
 
