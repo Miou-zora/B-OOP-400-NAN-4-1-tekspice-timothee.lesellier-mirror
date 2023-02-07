fe9701c2c313e57802bac33c95b79a32a42b1bef
Timothée Lesellier <timothee.lesellier@epitech.eu>
2023-02-07 09:25:09 +0100

    feat(core): adding IComponent class and the architecture for the project

---
+++ include/IComponent.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** B-OOP-400-NAN-4-1-tekspice-timothee.lesellier-mirror
+** File description:
+** IComponent
+*/
+
+
+#include <cstddef>
+
+namespace nts
+{
+    class IComponent {
+    public:
+            IComponent() = default;
+            virtual ~IComponent() = default;
+
+            virtual void simulate(std::size_t tick) = 0;
+            //virtual nts::Tristate compute(std::size_t pin) = 0;
+            virtual void setLink(std::size_t pin, nts::IComponent& other, std::size_t otherPin) = 0;
+    };
+
+}
