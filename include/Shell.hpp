e0e0e51d24aeb956aae1c6899ea1946cfcb7b430
Timothée Lesellier <timothee.lesellier@epitech.eu>
2023-02-14 14:57:01 +0100

    feat(core): adding the shell class for the input handle

---
+++ include/Shell.hpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2023
+** B-OOP-400-NAN-4-1-tekspice-timothee.lesellier-mirror
+** File description:
+** Shell
+*/
+
+#ifndef SHELL_HPP_
+    #define SHELL_HPP_
+
+    #include <iostream>
+    #include <string>
+    #include <fstream>
+    #include <list>
+
+    #include "IComponent.hpp"
+    #include "NTS.h"
+
+class Shell {
+    public:
+        Shell();
+        ~Shell();
+
+        void run();
+
+    protected:
+    private:
+
+        std::string getCommand();
+        std::string getCommandName(std::string command);
+        bool executeCommand(std::string command);
+
+        bool isCommandValid(std::string command);
+        bool isCommand(std::string command);
+        bool isInput(std::string command);
+        bool isOutput(std::string command);
+        bool isSimulate(std::string command);
+        bool isLoop(std::string command);
+        bool isExit(std::string command);
+        bool isDisplay(std::string command);
+};
+
+#endif /* !SHELL_HPP_ */
